79cafb9fb21caceb7bb5e9d3f14c704fbe8defed
b7song <b7song@student.cs.uwaterloo.ca>
2017-03-01 23:53:06 -0500

    Q7 Parquet

--- src/main/scala/ca/uwaterloo/cs/bigdata2017w/assignment5/Q7.scala
+++ src/main/scala/ca/uwaterloo/cs/bigdata2017w/assignment5/Q7.scala
@@ -105,66 +105,56 @@ object Q7 {
 
       val lineitemDF = sparkSession.read.parquet(args.input() + "/lineitem")
       val lineitemRDD = lineitemDF.rdd
+      val ordersDF = sparkSession.read.parquet(args.input() + "/orders")
+      val ordersRDD = ordersDF.rdd
+      val customerDF = sparkSession.read.parquet(args.input() + "/customer")
+      val customerRDD = customerDF.rdd
       val shipdate = args.date()
 
       val l = lineitemRDD
-      .filter(lines => lines(10).toString.contains(shipdate))
-      .keyBy(lines => (lines(8), lines(9))) // group by l_returnflag, l_linestatus
+      .filter(lines => lines(10).toString > shipdate)
+      .map(lines => {
+        (lines(0), ((lines(5).toString.toDouble) * (1 - lines(6).toString.toDouble)))
+      })
+      .keyBy(data => data._1)
+
+      val c = customerRDD
+      .flatMap(lines => List(new Pair(lines(0).toString, lines(1).toString)))
+
+      val cBroadcast = sc.broadcast(c.collectAsMap())
+
+      val o = ordersRDD
+      .filter(lines => lines(4).toString < shipdate)
+      .filter(lines => !cBroadcast.value.get(lines(1).toString).isEmpty)
+      .map(lines => {
+        // emit o_orderkey, o_orderdate, o_shippriority, c_name
+        (lines(0), lines(4), lines(7), cBroadcast.value.get(lines(1).toString).get)
+      })
+      .keyBy(data => data._1)
+
+      val grouped = o
+      .cogroup(l)
+      .filter(data => !data._2._1.isEmpty && !data._2._2.isEmpty)
+      .map(data => {
+        val l_orderkey = data._1
+        val revenue = data._2._2.map(x => x._2).sum
+        val o_orderdate = data._2._1.map(x => x._2).mkString(" ")
+        val o_shippriority = data._2._1.map(x => x._3).mkString(" ")
+        val c_name = data._2._1.map(x => x._4).mkString(" ")
+        (c_name, l_orderkey, o_orderdate, o_shippriority, revenue)
+      })
+      .keyBy(data => (data._1, data._2, data._3, data._4))
       .groupByKey()
+      .map(data => ((data._1._1, data._1._2, data._1._3, data._1._4), data._2.map(x => x._5).sum))
+      .sortBy(data => data._2, false)
+      .take(10)
 
-      val sum_qty = l
-      .map(data => {
-        data._2.iterator
-        .map(lines => lines(4).toString.toDouble)
-        .toList
-        .sum
-      }).sum()
-
-      val sum_base_price = l
-      .map(data => {
-        data._2.iterator
-        .map(lines => lines(5).toString.toDouble)
-        .toList
-        .sum
-      }).sum()
-
-      val sum_disc_price = l
-      .map(data => {
-        data._2.iterator
-        .map(lines => (lines(5).toString.toDouble) * (1 - lines(6).toString.toDouble))
-        .toList
-        .sum
-      }).sum()
-
-      val sum_charge = l
-      .map(data => {
-        data._2.iterator
-        .map(lines => (lines(5).toString.toDouble) * 
-                      (1 - lines(6).toString.toDouble) *
-                      (1 + lines(7).toString.toDouble))
-        .toList
-        .sum
-      }).sum()
-
-      val sum_disc = l
-      .map(data => {
-        data._2.iterator
-        .map(lines => lines(6).toString.toDouble)
-        .toList
-        .sum
-      }).sum()
-
-      for(data <- l.collect()) {
-        println("(" + data._1._1.toString + "," + 
+      for(data <- grouped) {
+        println("(" + data._1._1.toString + "," +
                 data._1._2.toString + "," +
-                sum_qty + "," +
-                sum_base_price + "," + 
-                sum_disc_price + "," + 
-                sum_charge + "," + 
-                sum_qty/data._2.count(x => true) + "," + 
-                sum_base_price/data._2.count(x => true) + "," +
-                sum_disc/data._2.count(x => true) + "," + 
-                data._2.count(x => true) + ")")
+                data._2 + "," +
+                data._1._3.toString + "," +
+                data._1._4.toString + ")")
       }
     }
   }
