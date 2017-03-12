067f90cd2656ff24ef507a132b57e2dc1c9ee506
b7song <b7song@student.cs.uwaterloo.ca>
2017-03-11 19:42:46 -0500

    Assignment 6 initial commit

---
+++ src/main/scala/ca/uwaterloo/cs/bigdata2017w/assignment6/Conf.scala
@@ -0,0 +1,27 @@
+package ca.uwaterloo.cs.bigdata2017w.assignment6
+
+import io.bespin.scala.util.Tokenizer
+import io.bespin.scala.util.WritableConversions
+
+import java.util.StringTokenizer
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable._
+
+import org.apache.hadoop.conf._
+import org.apache.hadoop.fs._
+import org.apache.hadoop.io._
+import org.apache.hadoop.mapreduce._
+import org.apache.hadoop.mapreduce.lib.input._
+import org.apache.hadoop.mapreduce.lib.output._
+import org.apache.hadoop.util.Tool
+import org.apache.hadoop.util.ToolRunner
+import org.apache.log4j._
+import org.rogach.scallop._
+
+class Conf(args: Seq[String]) extends ScallopConf(args) {
+  mainOptions = Seq(input, date)
+  val input = opt[String](descr = "input path", required = true)
+  val model = opt[String](descr = "output directory", required = false)
+  verify()
+}
---
+++ src/main/scala/ca/uwaterloo/cs/bigdata2017w/assignment6/TrainSpamClassifier.scala
@@ -0,0 +1,76 @@
+package ca.uwaterloo.cs.bigdata2017w.assignment6
+
+import io.bespin.scala.util.Tokenizer
+import io.bespin.scala.util.WritableConversions
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable._
+import scala.math._
+
+import org.apache.hadoop.conf._
+import org.apache.hadoop.fs._
+import org.apache.hadoop.io._
+import org.apache.hadoop.mapreduce._
+import org.apache.hadoop.mapreduce.lib.input._
+import org.apache.hadoop.mapreduce.lib.output._
+import org.apache.hadoop.util.Tool
+import org.apache.hadoop.util.ToolRunner
+import org.apache.log4j._
+import org.apache.spark.Partitioner
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.rogach.scallop._
+import tl.lin.data.pair.PairOfStrings
+import org.apache.spark.sql.SparkSession
+
+object TrainSpamClassifier {
+  val log = Logger.getLogger(getClass().getName())
+
+  def main(argv: Array[String]) {
+    val args = new Conf(argv)
+
+    log.info("Input: " + args.input())
+    log.info("Model: " + args.model())
+
+    val conf = new SparkConf().setAppName("TrainSpamClassifier")
+    val sc = new SparkContext(conf)
+    val textFile = sc.textFile(args.input())
+
+    val trained = textFile.map(line => {
+      // parse input
+      (0, (docid, isSpam, features))
+    }).groupByKey(1)
+    // run the trainer
+    trained.saveAsTextFile()
+
+    //----------------Begin boilerplate-------------
+    // w is the weight vector (make sure the variable is within scope)
+    val w = Map[Int, Double]()
+
+    // Scores a document based on its list of features.
+    def spamminess(features: Array[Int]) : Double = {
+      var score = 0d
+      features.foreach(f => if (w.contains(f)) score += w(f))
+      score
+    }
+
+    // This is the main learner:
+    val delta = 0.002
+
+    // For each instance...
+    val isSpam = ...   // label
+    val features = ... // feature vector of the training instance
+
+    // Update the weights as follows:
+    val score = spamminess(features)
+    val prob = 1.0 / (1 + exp(-score))
+    features.foreach(f => {
+      if (w.contains(f)) {
+        w(f) += (isSpam - prob) * delta
+      } else {
+        w(f) = (isSpam - prob) * delta
+      }
+    })
+    //------------------End boilerplate----------------
+  }
+}
